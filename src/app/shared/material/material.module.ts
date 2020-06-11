5c27cfd896539005c6c807d87252a42a0198fc6e
Srini25vasu <srini25vasu@gmail.com>
2020-06-11 20:01:21 +0200

    updated package.json

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule} from '@angular/material/button';
+import { MatAutocompleteModule} from '@angular/material/autocomplete';
+
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatAutocompleteModule
+  ]
+})
+export class MaterialModule { }
